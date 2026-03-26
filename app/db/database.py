a24767a3389962141b5327b63b778ebc246d1425
Kinjal Kathrecha <kinjalkathrecha2@gmail.com>
2026-03-26 11:42:17 +0530

    Database Setup (PostgreSQL)

---
+++ app/db/database.py
@@ -0,0 +1,10 @@
+import os
+from dotenv import load_dotenv
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker,declarative_base
+
+DATABASE_URL=os.getenv("DATABASE_URL")
+
+engine=create_engine(DATABASE_URL)
+SessionLocal=sessionmaker(bind=engine)
+base=declarative_base()
